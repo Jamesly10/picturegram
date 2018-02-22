90fa89c8831a76dc2a2102ca1ddf2b778a6a02d9
James <johndoe@example.com>
2018-02-22 10:05:45 -0500

    added comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-  class CommentsController < ApplicationController
   before_action :set_picture
 
   def create
@@ -15,5 +14,4 @@ class CommentsController < ApplicationController
     def comments_params
       params.require(:comment).permit(:body)
     end
-end
 end
